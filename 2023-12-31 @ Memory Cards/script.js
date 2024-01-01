42ffa5dfa4d894f7552597d147c3df7b1c54f2b5
Rodžer <rogermartitorne@gmail.com>
2024-01-01 19:04:42 +0100

    Fix of localStorage functioning.

--- 2023-12-31 @ Memory Cards/script.js
+++ 2023-12-31 @ Memory Cards/script.js
@@ -41,76 +41,93 @@ function handleSubmit(event) {
   closeForm();
 }
 
-// Modify the createCard function to add cards to the array and update localStorage
 function createCard(question, answer) {
-  // Create the card
-  const card = document.createElement("div");
-  if (cards.length === 0) {
-    card.className = "card active";
+  // Check if the card already exists in the cards array
+  const existingCard = cards.find(
+    (card) => card.innerText === `${question}\n${answer}`
+  );
+  if (existingCard) {
+    alert("This card already exists");
+  } else if (question.trim() === "" || answer.trim() === "") {
+    alert("Please enter both question and answer");
   } else {
-    for (let i = 0; i < cards.length; i++) {
-      if (cards[i].classList && cards[i].classList.contains("active")) {
-        card.className = "card";
+    // Create the card
+    const card = document.createElement("div");
+    if (cards.length === 0) {
+      card.className = "card active";
+    } else {
+      for (let i = 0; i < cards.length; i++) {
+        if (cards[i].classList && cards[i].classList.contains("active")) {
+          card.className = "card";
+        }
       }
     }
-  }
 
-  // Create the card content (question and answer)
-  const cardQuestion = document.createElement("div");
-  cardQuestion.className = "front";
-  cardQuestion.textContent = question;
+    // Create the card content (question and answer)
+    const cardQuestion = document.createElement("div");
+    cardQuestion.className = "front";
+    cardQuestion.textContent = question;
 
-  const cardAnswer = document.createElement("div");
-  cardAnswer.className = "back";
-  cardAnswer.textContent = answer;
+    const cardAnswer = document.createElement("div");
+    cardAnswer.className = "back";
+    cardAnswer.textContent = answer;
 
-  // Add the card content to the card
-  card.appendChild(cardQuestion);
-  card.appendChild(cardAnswer);
+    // Add the card content to the card
+    card.appendChild(cardQuestion);
+    card.appendChild(cardAnswer);
 
-  // Add the card to the container
-  cardsContainer.appendChild(card);
+    // Add the card to the array
+    cards.push(card)
+    console.log("Card created", cards);
 
-  // Variable to know the card status (rotation)
-  let cardStatus = true;
-
-  card.addEventListener("click", function () {
-    card.classList.toggle("show-answer");
-
-    if (cardStatus) {
-      // Hide the question
-      cardQuestion.style.display = "none";
-
-      // Set delay of 0.2s to show the answer
-      setTimeout(() => {
-        cardAnswer.style.backfaceVisibility = "visible";
-      }, 200);
-
-      // Change cardStatus
-      cardStatus = false;
-    } else {
-      // Hide the answer
-      cardAnswer.style.backfaceVisibility = "hidden";
-
-      // Set delay of 0.2s to show the question
-      setTimeout(() => {
-        cardQuestion.style.display = "flex";
-      }, 200);
-
-      // Change cardStatus
-      cardStatus = true;
+    // Save the card content to localStorage if it doesn't already exist
+    const existingStorageCard = storageCards.find(
+      (card) => card.question === question && card.answer === answer
+    );
+    if (!existingStorageCard) {
+      storageCards.push({ question, answer });
+      localStorage.setItem("cards", JSON.stringify(storageCards));
+      console.log("Card added to storageCards array", storageCards);
     }
-  });
 
-  // Save the card content to localStorage
-  storageCards.push({ question, answer });
-  localStorage.setItem("cards", JSON.stringify(storageCards));
+    // Add the card to the container
+    cardsContainer.appendChild(card);
 
-  // Add the card to the array
-  cards.push(card);
+    // Variable to know the card status (rotation)
+    let cardStatus = true;
 
-  // Update the navigation information
-  updateNavigation();
+    // Card flip animation + logic
+    card.addEventListener("click", function () {
+      card.classList.toggle("show-answer");
+
+      if (cardStatus) {
+        // Hide the question
+        cardQuestion.style.display = "none";
+
+        // Set delay of 0.2s to show the answer
+        setTimeout(() => {
+          cardAnswer.style.backfaceVisibility = "visible";
+        }, 200);
+
+        // Change cardStatus
+        cardStatus = false;
+      } else {
+        // Hide the answer
+        cardAnswer.style.backfaceVisibility = "hidden";
+
+        // Set delay of 0.2s to show the question
+        setTimeout(() => {
+          cardQuestion.style.display = "flex";
+        }, 200);
+
+        // Change cardStatus
+        cardStatus = true;
+      }
+    });
+
+    // Update the navigation information
+    updateNavigation();
+  }
 }
 
 // Function to update the navigation information
@@ -140,7 +157,11 @@ function clearCards() {
 
   // Remove cards from localStorage
   storageCards.length = 0;
-  localStorage.removeItem("cards");
+  localStorage.clear();
+
+  // Show arrays
+  console.log("Cleared cards array", cards);
+  console.log("Cleared storageCards array", storageCards);
 
   // Update the navigation information
   updateNavigation();
@@ -194,9 +215,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Retrieve the card content from localStorage
   storageCards = JSON.parse(localStorage.getItem("cards")) || [];
-
-  // Clear the existing cards in the container
-  cardsContainer.innerHTML = '';
 
   // Recreate the cards from the stored content
   storageCards.forEach(({ question, answer }) => {
